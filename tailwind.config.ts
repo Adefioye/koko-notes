bd33019f0ed2811ac67f4b61c5338f489ea31755
Adefioye <abdulhakeemadefioye@gmail.com>
2023-12-14 01:48:18 -0600

    Add plugins in tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,27 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: [
+          "Nunito Sans",
+          "Nunito Sans Fallback",
+          ...defaultTheme.fontFamily.sans,
+        ],
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [
+    require( 'tailwindcss' ),
+    require( 'postcss' ),
+    require( 'autoprefixer' ),
+  ],
+};
+export default config;
