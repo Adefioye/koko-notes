2f52ae35c733595e39f4922c1b72789828e20596
Adefioye <abdulhakeemadefioye@gmail.com>
2023-12-16 17:48:39 -0600

    Create routing for /kody, /kody/notes, /kody/notes/:id

---
+++ app/users/kody/page.tsx
@@ -0,0 +1,7 @@
+export default function KodyProfileRoute() {
+  return (
+    <div className="container mb-48 mt-36 border-4 border-green-500">
+      <h1 className="text-h1">Kody</h1>
+    </div>
+  );
+}
