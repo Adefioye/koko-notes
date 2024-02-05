eabc80d4b84c7bd122bc31ebed697863b1a9adb8
Adefioye <abdulhakeemadefioye@gmail.com>
2024-02-04 20:47:00 -0600

    Add nested querying to update seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -13,49 +13,40 @@ async function addNoteImages() {
   await prisma.user.deleteMany();
 
   // Create new user koko
-  const koko = await prisma.user.create({
+  await prisma.user.create({
     data: {
       email: "koko@example.com",
       username: "koko",
       name: "Koko",
-    },
-  });
-  const firstNote = await prisma.note.create({
-    data: {
-      id: "d27a197e",
-      title: "Basic koala facts",
-      content:
-        "Koalas are found in the eucalyptus forests of eastern Australia. They have grey fur with a cream-coloured chest, and strong, clawed feet, perfect for living in the branches of trees!",
-      ownerId: koko.id,
-    },
-  });
-
-  if (!firstNote) {
-    throw new Error("You need to have a note in the database first");
-  }
-
-  await prisma.note.update({
-    where: { id: firstNote.id },
-    data: {
-      images: {
+      notes: {
         create: [
           {
-            altText: "an adorable koala cartoon illustration",
-            contentType: "image/png",
-            blob: await fs.promises.readFile(
-              path.join(cwd, relativePathOfFile, "cute-koala.png")
-            ),
-            createdAt: new Date(),
-            updatedAt: new Date(),
-          },
-          {
-            altText: "a cartoon illustration of a koala in a tree eating",
-            contentType: "image/png",
-            blob: await fs.promises.readFile(
-              path.join(cwd, relativePathOfFile, "koala-eating.png")
-            ),
-            createdAt: new Date(),
-            updatedAt: new Date(),
+            id: "d27a197e",
+            title: "Basic koala facts",
+            content:
+              "Koalas are found in the eucalyptus forests of eastern Australia. They have grey fur with a cream-coloured chest, and strong, clawed feet, perfect for living in the branches of trees!",
+            images: {
+              create: [
+                {
+                  altText: "an adorable koala cartoon illustration",
+                  contentType: "image/png",
+                  blob: await fs.promises.readFile(
+                    path.join(cwd, relativePathOfFile, "cute-koala.png")
+                  ),
+                  createdAt: new Date(),
+                  updatedAt: new Date(),
+                },
+                {
+                  altText: "a cartoon illustration of a koala in a tree eating",
+                  contentType: "image/png",
+                  blob: await fs.promises.readFile(
+                    path.join(cwd, relativePathOfFile, "koala-eating.png")
+                  ),
+                  createdAt: new Date(),
+                  updatedAt: new Date(),
+                },
+              ],
+            },
           },
         ],
       },
